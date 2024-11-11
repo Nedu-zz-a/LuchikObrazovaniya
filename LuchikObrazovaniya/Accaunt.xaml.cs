466783a73c4979df9b2ac6f38924dcda2753300e
dinar <dinar@DESKTOP-V11GBGJ>
2024-11-11 06:42:01 +0300

    Добавьте файлы проекта.

---
+++ LuchikObrazovaniya/Accaunt.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Automation.Peers;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static LuchikObrazovaniya.Accaunt;
+
+namespace LuchikObrazovaniya
+{
+    /// <summary>
+    /// Логика взаимодействия для Accaunt.xaml
+    /// </summary>
+    public partial class Accaunt : Window
+    {
+
+        private List<Student> students;
+        public Accaunt()
+        {
+            InitializeComponent();
+            MainWindow mainWindow = new MainWindow();
+            FIO.Text = MainWindow.teacherFio;
+            Napravlenie.Text = MainWindow.teacherNapr;
+            if (MainWindow.teacherId == 0 || MainWindow.teacherId == 1)
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    FIO_Students.Items.Add(MainWindow.Students[i]);
+                }
+                List<Student> students = new List<Student>
+                {
+
+                    new Student { Name = MainWindow.Students[0], Grades = new List<int> { 5  }},
+                    new Student { Name = MainWindow.Students[1], Grades = new List<int> { 5  }},
+                    new Student { Name = MainWindow.Students[2], Grades = new List < int > { 5, 4 }},
+                    new Student { Name = MainWindow.Students[3], Grades = new List < int > { 5, 4 }},
+                    new Student { Name = MainWindow.Students[4], Grades = new List < int > { 5 }},
+                    new Student { Name = MainWindow.Students[5], Grades = new List<int> { 5, 4 }},
+                };
+                gradesDataGrid.ItemsSource = students;
+            }
+
+            if (MainWindow.teacherId == 2 || MainWindow.teacherId == 3)
+            {
+                for (int i = 6; i < 12; i++)
+                {
+                    FIO_Students.Items.Add(MainWindow.Students[i]);
+                }
+                List<Student> students = new List<Student>
+                {
+
+                    new Student { Name = MainWindow.Students[6], Grades = new List<int> { 5 }},
+                    new Student { Name = MainWindow.Students[7], Grades = new List<int> { 5 }},
+                    new Student { Name = MainWindow.Students[8], Grades = new List < int > { 5 }},
+                    new Student { Name = MainWindow.Students[9], Grades = new List < int > { 5 }},
+                    new Student { Name = MainWindow.Students[10], Grades = new List < int > { 5 }},
+                    new Student { Name = MainWindow.Students[11], Grades = new List < int > { 5 }},
+                };
+                gradesDataGrid.ItemsSource = students;
+
+            }
+            if (MainWindow.teacherId == 4 || MainWindow.teacherId == 5)
+            {
+                for (int i = 12; i < 18; i++)
+                {
+                    FIO_Students.Items.Add(MainWindow.Students[i]);
+                }
+
+                List<Student> students = new List<Student>
+                {
+
+                    new Student { Name = MainWindow.Students[12], Grades = new List < int > { 5}},
+                    new Student { Name = MainWindow.Students[13], Grades = new List < int > { 5}},
+                    new Student { Name = MainWindow.Students[14], Grades = new List < int > { 5}},
+                    new Student { Name = MainWindow.Students[15], Grades = new List < int > { 5} },
+                    new Student { Name = MainWindow.Students[16], Grades = new List < int > { 5 } },
+                    new Student { Name = MainWindow.Students[17], Grades = new List < int > { 5}},
+                };
+                gradesDataGrid.ItemsSource = students;
+            }
+        }
+
+        private void quit_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void getmark_Click(object sender, RoutedEventArgs e)
+        {
+            string studentName = FIO_Students.Text;
+            if (int.TryParse(Marks.Text, out int newGrade))
+            {
+                // Находим ученика по имени
+                var student = students.FirstOrDefault(s => s.Name == studentName);
+                if (student != null)
+                {
+                    student.Grades.Add(newGrade); // Добавляем новую оценку
+                    gradesDataGrid.Items.Refresh(); // Обновляем таблицу
+                }
+                else
+                {
+                    MessageBox.Show("Ученик не найден.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите корректное значение для оценки.");
+            }
+        } 
+    }
+
+
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public List<int> Grades { get; set; } = new List<int>(); // Инициализируем список оценок
+
+        // Свойство для отображения всех оценок в одной строке
+        public string GradesAsString => string.Join(", ", Grades);
+    }
+
+}
+
---
+++ LuchikObrazovaniya/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LuchikObrazovaniya
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public static int teacherId { set; get; }
+        public static string teacherFio { set; get; }
+        public static string teacherNapr { set; get; }
+        public static string[] Students { set; get; } = { "Маркина Милана Денисовна", "Новикова Александра Давидовна", "Васильев Макар Семёнович", "Новикова Алсу Дмитриевна", "Федотова Милана Максимовна", "Фетисова Юлия Васильевна", "Кузнецов Виктор Константинович", "Дорофеев Степан Маркович", "Фомин Кирилл Андреевич", "Горшкова Александра Львовна", "Волков Максим Александрович", "Зиновьева Анастасия Ивановна", "Хохлова Софья Ивановна", "Румянцева Александра Георгиевна", "Беляев Ярослав Егорович", "Русакова Мария Всеволодовна", "Кузнецова Алина Артёмовна", "Марков Роман Давидович" };
+
+
+        int[,] login_password = { { 1111, 1111 }, { 2222, 2222 }, { 3333, 3333 }, { 4444, 4444 }, { 5555, 5555 }, { 6666, 6666 } };
+
+        string[,] FIO_Napr = { { "Елена Николаевна", "Сетевик" }, { "Ярослава Сергеевна", "Сетевик" }, { "Наталья Струженикова", "Программист" }, { "Ольга Кружок", "Программист" }, {"Анна Валерьевна", "Безопасик" }, { "Мария Никитивна", "Безопасник" } };
+
+        private void log_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                if (loginTeacher.Text == login_password[i,0].ToString() && PasswordTeacher.Text == login_password[i,1].ToString())
+                {
+                    teacherId = i;
+                    teacherFio = FIO_Napr[i, 0];
+                    teacherNapr = FIO_Napr[i, 1];
+                    Accaunt accaunt = new Accaunt();
+                    accaunt.Show();
+                    this.Close();
+                    break;
+                }
+                else
+                {
+                    if (i == 5)
+                    {
+                        MessageBox.Show("Неверный логин или пароль", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                }
+            }
+        }
+    }
+}
